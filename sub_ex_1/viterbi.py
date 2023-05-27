fd9b217ba986cf287d837b13f50d06d257eeef16
Tony Kristaki <tonykristaki@gmail.com>
2023-05-27 14:12:25 +0300

    Finish exercise 1

--- sub_ex_1/viterbi.py
+++ sub_ex_1/viterbi.py
@@ -4,29 +4,17 @@ from probabilities import p_start, p_states, p_symbol
 
 def get_scores(seq):
     score_grid = np.zeros((len(p_start),len(seq)))
-    max_prev_prob = float('-inf')
-    prev_state = None
 
     for col, symbol in enumerate(seq):
         if col == 0:
             for row, state in enumerate(p_states):
-                score_grid[row, col] = math.log(p_start[state]) + math.log(p_symbol[state][symbol])
-                if score_grid[row, col] > max_prev_prob:
-                    max_prev_prob = score_grid[row, col]
-                    prev_state = state
+                score_grid[row, col] = math.log2(p_start[state]) + math.log2(p_symbol[state][symbol])
             continue
 
-        temp_max_prev_prob = float('-inf')
-        temp_prev_state = None
-
         for row, state in enumerate(p_states):
-            score_grid[row, col] = max_prev_prob + math.log(p_states[state][prev_state]) + math.log(p_symbol[state][symbol])
-            if score_grid[row, col] > temp_max_prev_prob:
-                    temp_max_prev_prob = score_grid[row, col]
-                    temp_prev_state = state
+            score_grid[row, col] = math.log2(p_symbol[state][symbol]) + \
+                  max(math.log2(p_states['a'][state]) + score_grid[0, col-1], math.log2(p_states['b'][state]) + score_grid[1, col-1])
 
-        max_prev_prob = temp_max_prev_prob
-        prev_state = temp_prev_state
 
     return score_grid
 
@@ -34,7 +22,6 @@ def get_path(score_grid, seq):
     path = []
 
     for col in reversed(range(len(seq))):
-        symbol = seq[col]
         max = float('-inf')
         selected_state = None
 
